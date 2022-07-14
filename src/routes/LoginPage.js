8a83181d27be3fb3557102d1cb7df04da5ad1628
dhruvroytalukdar <dhruvroy8@gmail.com>
2022-07-14 13:19:58 +0530

    padding adjustment in login page

--- src/routes/LoginPage.js
+++ src/routes/LoginPage.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 export default function LoginPage() {
   return (
     <div className="h-full flex items-center justify-center">
-      <div className="flex flex-col mx-4 md:mx-0 md:w-1/4 px-12 py-10 space-y-5 items-center border-2 rounded-sm">
+      <div className="flex flex-col mx-4 md:mx-0 px-12 py-10 w-full md:w-1/4 space-y-4 items-center border-2 rounded-sm">
         <p className="text-4xl font-bold py-5">Login</p>
         <input
           type="text"
           placeholder="johndoe@gmail.com"
-          className="py-2 px-3 focus:outline-1 outline-gray-500 text-lg bg-slate-100 rounded-sm"
+          className="w-full py-2 px-3 focus:outline-1 outline-gray-500 text-lg bg-slate-100 rounded-sm"
         />
         <input
           type="password"
           placeholder="Password"
-          className="py-2 px-3 focus:outline-1 outline-gray-500 text-lg bg-slate-100 rounded-sm"
+          className="w-full py-2 px-3 focus:outline-1 outline-gray-500 text-lg bg-slate-100 rounded-sm"
         />
         <button className="py-2 px-3 bg-green-500 w-full text-white text-lg font-semibold rounded-sm">
           SignIn
