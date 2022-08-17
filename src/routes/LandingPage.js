5771e0665f47586192ee50f65a38fa354e2b7c20
dhruvroytalukdar <dhruvroy8@gmail.com>
2022-08-17 14:02:23 +0530

    refactor: improved the front page

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -4,43 +4,48 @@ import Footer from "../components/Footer";
 export default function LandingPage() {
   return (
     <>
-      <div className="h-full px-4 md:px-10 md:flex items-center justify-between my-4 md:my-0">
-        <p className="md:w-1/2 md:pr-10 text-lg">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+      <div className="h-full px-4 md:px-10 md:flex flex-col items-start justify-evenly my-4 md:my-0">
+        <p className="text-2xl font-light">
+          <p className="inline font-black">Dentify</p> is a dental clinic
+          management software where you can manage your patients, doctors and
+          other staffs. Along with that you can create appointments and keep
+          track of payments as well.
         </p>
-        <div className="md:w-1/2 md:pl-10 space-y-5">
-          <div className="md:w-5/6">
-            <p className="text-xl font-semibold py-2">Feature 1</p>
-            <p className="text-lg">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout.
-            </p>
-          </div>
+        <div className="flex flex-col">
+          <p className="text-2xl font-bold pb-8">KEY FEATURES</p>
+          <div className="md:flex md:items-center">
+            <div className="md:w-5/6">
+              <p className="text-xl font-semibold py-2">
+                Create and track appointments
+              </p>
+              <p className="text-lg">
+                You will be able to create appointments for each doctors and
+                track them.
+              </p>
+            </div>
 
-          <div className="md:w-5/6">
-            <p className="text-xl font-semibold py-2">Feature 2</p>
-            <p className="text-lg">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout.
-            </p>
-          </div>
+            <div className="md:w-5/6">
+              <p className="text-xl font-semibold py-2">User roles</p>
+              <p className="text-lg">
+                Admin can assign roles to their members which will determine
+                what he/she can do in the Dentify.
+              </p>
+            </div>
 
-          <div className="md:w-5/6">
-            <p className="text-xl font-semibold py-2">Feature 3</p>
-            <p className="text-lg">
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout.
-            </p>
+            <div className="md:w-5/6">
+              <p className="text-xl font-semibold py-2">Report generation</p>
+              <p className="text-lg">
+                You can monitor and store mouth details of each patients and can
+                generate a report which can be saved as pdf.
+              </p>
+            </div>
           </div>
         </div>
+        <p className="text-xl">
+          To <p className="inline font-bold">Get Started</p> register as your
+          respective role and fill out all details. After that contact your
+          organization admin to accept your profile.
+        </p>
       </div>
       <Footer />
     </>
